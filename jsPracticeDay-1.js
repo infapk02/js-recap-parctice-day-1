ff6057f80627694571286edcea00870b4bab6eda
ahmedinfa <info.infa02@gmail.com>
2025-12-12 10:50:58 +0600

    js variable practice

---
+++ jsPracticeDay-1.js
@@ -0,0 +1,24 @@
+// problem 1
+var myBalance = 1000;
+var applePrice = 300;
+var orangePrice = 400;
+
+var totalPrice = applePrice + orangePrice;
+console.log(myBalance - totalPrice);
+
+// problem 2
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.5;
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+var averageMarks = totalMarks / 5;
+var averageMarksFixed = parseFloat(averageMarks.toFixed(2));
+console.log(averageMarksFixed);
+
+// problem 3 = divide the given number and show the remainder
+var num = 119;
+var remainder = num % 5;
+console.log(remainder);
